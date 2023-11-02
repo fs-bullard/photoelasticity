b7b6d13076ff0e77d5894075796dd21e30844196
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2023-11-02 14:38:16 +0000

    moved load_raw_img into new file

---
+++ functions.py
@@ -0,0 +1,11 @@
+import numpy as np
+import rawpy
+
+def load_raw_img(path: str) -> np.ndarray:
+    """
+    Returns the raw image at path as ndarray
+    """
+    with rawpy.imread(path) as raw_img:
+        rgb_img = raw_img.postprocess()
+    
+    return rgb_img
