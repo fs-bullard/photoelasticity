e9b9d14201d09994a363c84627b4c087d4d819cd
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2023-11-03 16:42:50 +0000

    tweaked, used arctan 2

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import cv2 as cv
 import numpy as np
 import rawpy
 
@@ -12,3 +13,9 @@ def load_raw_img(path: str) -> np.ndarray:
 
 def rad_to_deg(a):
     return 180*a/np.pi
+
+def mask_circ_img(img: np.ndarray, center: (int, int), radius: int) -> np.ndarray:
+    mask = np.zeros(img.shape, dtype=img.dtype)
+    mask = cv.circle(mask, center, radius, 255, -1)
+
+    return cv.bitwise_and(img, mask)
