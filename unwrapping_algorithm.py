ab281ddc965ff28f218bf3914dcd0a88526ec611
zebsummerfield <33784478+zebsummerfield@users.noreply.github.com>
2023-11-13 18:08:07 +0000

    working algorithm

--- unwrapping_algorithm.py
+++ unwrapping_algorithm.py
@@ -1,21 +1,48 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-radius = 1500
-centre = (3000,2000)
-stack = [(centre[0]-radius, centre[1]), (centre[0]+radius, centre[1])]
-dummy = np.zeroes(6000,4000)
-phase_map = np.zeroes(6000, 4000)
+seed = (50,0)
+stack = [(seed[0], seed[1]), (seed[0], seed[1])]
+dummy = np.zeros((100,100))
+dummy[:,:] = -1
+
+phase_map = np.zeros((100, 100))
+x = 0
+dx = 4 * np.pi / 100
+for i in range(100):
+    phase_map[:,i] = x
+    x += dx
+phase_map = phase_map % (2 * np.pi)
+
+# plt.imshow(phase_map, cmap='gray')
+# plt.show()
 
 def unwrap(pixel, previous):
     return ((phase_map[pixel] - dummy[previous] + np.pi) % (2 * np.pi)) + dummy[previous] - np.pi
 
+# x = np.unwrap(phase_map)
+# plt.imshow(x, cmap='gray')
+# plt.show()
+
 while stack:
     x,y = stack[0]
-    unwrapped = [coord for coord in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)] if dummy[coord] == -1]
+    neighbours = [coord for coord in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+                  if coord[0]<100 and coord[0]>=0 and coord[1]<100 and coord[1]>=0]
+    unwrapped = [coord for coord in neighbours if dummy[coord] == -1]
+
     for pixel in unwrapped:
         dummy[pixel] = unwrap(pixel, stack[0])
+
         x1,y1 = pixel
-        for coord in [(x1-1,y1),(x1+1,y1),(x1,y1-1),(x1,y1+1)]:
+        new_neighbours = [coord for coord in [(x1-1,y1),(x1+1,y1),(x1,y1-1),(x1,y1+1)]
+                          if coord[0]<100 and coord[0]>=0 and coord[1]<100 and coord[1]>=0]
+        neighbour_wrapped = False
+        for coord in new_neighbours:
             if dummy[coord] == -1:
-                stack.append(coord)
+                neighbour_wrapped = True
+        if neighbour_wrapped:
+            stack.append(pixel)
     stack.pop(0)
+
+plt.imshow(dummy, cmap='gray')
+plt.show()
