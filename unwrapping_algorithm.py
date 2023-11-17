804754af4fac7ed10c686e419a7f1a1b3dca1cd3
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2023-11-17 16:33:04 +0000

    getting some kind of result, stack getting stuck

--- unwrapping_algorithm.py
+++ unwrapping_algorithm.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 import pdb
+from functions import mask_circ_img
 
 
 # seed = (50,0)
@@ -32,9 +33,9 @@ import pdb
 #     x,y = stack[0]
 #     neighbours = [coord for coord in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
 #                   if coord[0]<100 and coord[0]>=0 and coord[1]<100 and coord[1]>=0]
-#     unwrapped = [coord for coord in neighbours if dummy[coord] == -1]
+#     wrapped = [coord for coord in neighbours if dummy[coord] == -1]
 
-#     for pixel in unwrapped:
+#     for pixel in wrapped:
 #         dummy[pixel] = unwrap(pixel, stack[0])
 
 #         x1,y1 = pixel
@@ -58,7 +59,7 @@ def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
     input stack is list of isotropic points
     """
 
-    dummy = np.zeros(img.shape, dtype=np.int8)
+    dummy = np.zeros(img.shape)
 
     dummy[:,:] = -2
 
@@ -66,24 +67,44 @@ def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
     # -2 outside the mask, -1 inside the mask
     cv.circle(dummy, center, radius, -1, -1)
 
-    print(dummy[1500, 3000])
+    # print(dummy[1500, 3000])
 
     def unwrap(pixel, previous) -> float:
         """
         Returns the unwrap phase of pixel given the phase of its neighbour
         """
-        return ((img[pixel] - dummy[previous] + np.pi) % (2 * np.pi)) + dummy[previous] - np.pi
+        scale = 127.5
+        # import pdb
+        # pdb.set_trace()
+        return ((img[pixel] - dummy[previous] + scale) % (2*scale)) + dummy[previous] - scale
+    
+    for seed in stack:
+        dummy[seed] = img[seed]
+    print(img[stack[0]], img[stack[1]])
 
+    
+    iteration = 0
     while stack:
         x, y = stack[0]
         neighbours = [coord for coord in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]]
-        unwrapped = [coord for coord in neighbours if dummy[coord] == -1]
+        wrapped = [coord for coord in neighbours if dummy[coord] == -1]
+        # pdb.set_trace()
+        # print(wrapped)
 
-        print(unwrapped)
+        for pixel in wrapped:
+            if iteration % 50000 == 0:
+                # plt.imshow(dummy, cmap='gray')
+                # plt.show()
+                print(iteration, len(stack))
+            if iteration > 3500000:
+                pdb.set_trace()
+                print(stack)
+                stack = [1]
+                break
 
-        for pixel in unwrapped:
             dummy[pixel] = unwrap(pixel, stack[0])
-            print(dummy[pixel])
+            iteration += 1
+            # print(dummy[pixel])
             
 
             x1, y1 = pixel
@@ -96,20 +117,26 @@ def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
                 stack.append(pixel)
         stack.pop(0)
 
-    return dummy
+    return abs(dummy)/ 255
 
 if __name__ == "__main__":
     # Load img
     filename = 'isochromatic_processed.jpg'
-    img = cv.imread(filename, cv.IMREAD_GRAYSCALE)
-
+    img = cv.medianBlur(cv.imread(filename, cv.IMREAD_GRAYSCALE), 5) 
+    img[964, 2437] = 200
+    # import pdb
+    # pdb.set_trace()
+    r = 1050
+    center = (3030, 1770)
+    # r = 250
+    # center = (335, 335)
+    plt.imshow(mask_circ_img(img, center, r), cmap='gray')
+    plt.show()
     # Set stack as isotropic points
-    stack = [(1770, 3030 - 1050)]
+    stack = [(center[1], center[0] - r), (center[1], center[0] + r)]
 
-    img_unwrapped = phase_unwrap(img, stack, (3030, 1770), 1050)
-    # img_unwrapped = np.unwrap(img)
-
-    print(img_unwrapped)
+    img_unwrapped = phase_unwrap(img, stack, center, r)
+    # img_unwrapped = np.unwrap(mask_circ_img(img, center, r), period=255)
 
     plt.imshow(img_unwrapped, cmap='gray')
     plt.show()
