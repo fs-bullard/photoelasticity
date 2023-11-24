01c58481e76997294e35bf3fe954fe3b1717ce26
zebsummerfield <33784478+zebsummerfield@users.noreply.github.com>
2023-11-24 14:06:49 +0000

    adding unwrapping of isoclinic

--- unwrapping_isoclinic.py
+++ unwrapping_isoclinic.py
@@ -4,7 +4,7 @@ import cv2 as cv
 import pdb
 from functions import mask_circ_img
 
-def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
+def phase_unwrap_isoclinic(img: np.ndarray, stack, center, radius) -> np.ndarray:
     """
     input stack is list of isotropic points
     """
@@ -15,7 +15,7 @@ def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
 
     # Need to change if shape not circle
     # -2 outside the mask, -1 inside the mask
-    cv.circle(dummy, center, radius, -1, -1)
+    cv.circle(dummy, center, radius, np.inf, -1)
 
     # print(dummy[1500, 3000])
 
@@ -41,12 +41,12 @@ def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
     while stack:
         x, y = stack[0]
         neighbours = [coord for coord in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]]
-        wrapped = [coord for coord in neighbours if dummy[coord] == -1]
+        wrapped = [coord for coord in neighbours if dummy[coord] == np.inf]
         # pdb.set_trace()
         # print(wrapped)
 
         for pixel in wrapped:
-            if iteration % 50000 == 0:
+            if iteration % 300000 == 0:
                 # plt.imshow(dummy, cmap='gray')
                 # plt.show()
                 print(iteration, len(stack))
@@ -65,13 +65,13 @@ def phase_unwrap(img: np.ndarray, stack, center, radius) -> np.ndarray:
             new_neighbours = [coord for coord in [(x1-1,y1),(x1+1,y1),(x1,y1-1),(x1,y1+1)]]
             neighbour_wrapped = False
             for coord in new_neighbours:
-                if dummy[coord] == -1:
+                if dummy[coord] == np.inf:
                     neighbour_wrapped = True
             if neighbour_wrapped:
                 stack.append(pixel)
         stack.pop(0)
 
-    return abs(dummy)/ 255
+    return (((abs(dummy)/ 255) % 2) - 1) * (np.pi/4)
 
 if __name__ == "__main__":
     # Load img
@@ -79,15 +79,15 @@ if __name__ == "__main__":
     img = cv.medianBlur(cv.imread(filename, cv.IMREAD_GRAYSCALE), 5) 
     r = 1050
     center = (3030, 1770)
-    # r = 250
-    # center = (335, 335)
     plt.imshow(mask_circ_img(img, center, r), cmap='gray')
     plt.show()
     # Set stack as isotropic points
-    stack = [(2750,2750), (850, 3400)]
+    # stack = [(2600,3500), (1000, 2400)]
+    stack = [(center[1], center[0] - r), (center[1], center[0] + r)]
 
-    img_unwrapped = phase_unwrap(img, stack, center, r)
+    img_unwrapped = phase_unwrap_isoclinic(img, stack, center, r)
     # img_unwrapped = np.unwrap(mask_circ_img(img, center, r), period=255)
 
+    plt.imsave('isoclinic_unwrapped.jpg', img_unwrapped, cmap='gray')
     plt.imshow(img_unwrapped, cmap='gray')
     plt.show()
